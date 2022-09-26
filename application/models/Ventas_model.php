1ea2dbb00b203fd69d9d8a94b4301818d0cabeb7
efrainnn79 <caracara.efrain.791@gmail.com>
2022-09-26 18:52:23 -0400

    conexion con la base de datos Ventas_model

--- application/models/Ventas_model.php
+++ application/models/Ventas_model.php
@@ -3,14 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ventas_model extends CI_ModeL {
 
-
   public function lista()
     {
         $this->db->select('*');
         $this->db->from('vwventas');
         return $this->db->get();
     }
-
-
-  
 }
