efc1dbf27f0e224c51b2e904ec48bd51a39297a8
efrainnn79 <caracara.efrain.791@gmail.com>
2022-10-26 03:11:49 -0400

    agregando a controladores el formulario para insertar apellidos

--- application/controllers/productos.php
+++ application/controllers/productos.php
@@ -22,3 +22,27 @@ class Estudiante extends CI_Controller {
             redirect('usuarios/panel','refresh');
         }
 	}
+    public function guest()
+	{
+        if ($this->session->userdata('tipo')=='guest')
+        {
+        $this->load->view('inc/header');
+		$this->load->view('panelguest');
+        $this->load->view('inc/footer');
+        }
+	}
+	public function agregar()
+	{
+
+        $this->load->view('inc/header');
+		$this->load->view('formulario');
+        $this->load->view('inc/footer');
+	}
+    public function agregarbd()
+	{
+        $data['nombre']=$_POST['nombre'];
+        $data['primerApellido']=$_POST['primerapellido'];
+        $data['segundoApellido']=$_POST['segundoapellido'];
+
+        $lista=$this->estudiante_model->agregarestudiante($data);
+        redirect('estudiante/index','refresh');
